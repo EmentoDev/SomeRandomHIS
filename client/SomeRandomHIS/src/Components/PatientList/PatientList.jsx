d6c58d26884e56ed94f30b6d73aae72883479faf
Chris Holm Roelsgaard <chris@emento.dk>
2023-06-08 23:26:19 +0200

    marking selected patient

--- client/SomeRandomHIS/src/Components/PatientList/PatientList.jsx
+++ client/SomeRandomHIS/src/Components/PatientList/PatientList.jsx
@@ -2,67 +2,97 @@ import { useEffect, useState } from 'react';
 import { fhirVersions, Patient } from 'fhir-react';
 import FHIR from 'fhirclient';
 import './PatientList.css';
-import { useViewStore } from '../../store';
+import { useViewStore, usePatientStore } from '../../store';
 import { VIEWS } from '../../constants';
 import NEW_PATIENT from './NewPatient.json';
 
 const fhirclient = FHIR.client({
-  serverUrl: 'https://76ffhj0j-5173.euw.devtunnels.ms/4_0_0'
+    serverUrl: 'https://76ffhj0j-5173.euw.devtunnels.ms/4_0_0',
 });
 
-const PatientCard = ({patientId}) => {
-  const [patient, setPatient] = useState({});
-  const setView = useViewStore(state => state.set);
+const PatientCard = ({ patientId }) => {
+    const [patient, setPatient] = useState({});
+    const setView = useViewStore((state) => state.set);
+    const setSelectedPatient = usePatientStore((state) => state.set);
+    const selectedPatient = usePatientStore((state) => state.patient);
 
-  useEffect(() => {
-    const getPatient = async () => {
-      const data = await fhirclient.request(`Patient/${patientId}`);
-      setPatient(data);
-    };
-    getPatient();
-  }, [patientId]);
+    useEffect(() => {
+        const getPatient = async () => {
+            const data = await fhirclient.request(`Patient/${patientId}`);
+            setPatient(data);
+        };
+        getPatient();
+    }, [patientId]);
 
-  return (
-    <li className='item'>
-      <Patient
-        style={{width: '100%'}}
-        fhirResource={patient}
-        fhirVersion={fhirVersions.R4}
-        patientSimple={false}
-        rawOnClick={() => {setView(VIEWS.RESOURCE_EDIT, patient);}}
-      />
-      <div style={{margin: '-5px 0 20px 15px'}}>
-        <span className='patientActionButton' onClick={() => {setView(VIEWS.APPOINTMENT_LIST, patient)}}>show Appointments</span>
-        <span className='patientActionButton' onClick={() => {setView(VIEWS.DOCUMENT_REFERENCE_LIST, patient)}}>show Documents</span>
-      </div>
-    </li>
-  );
+    return (
+        <li className="item" style={{ backgroundColor: selectedPatient?.id === patient.id ? '#ffffff50' : 'transparent', paddingBottom: '1px' }}>
+            <Patient
+                style={{ width: '100%' }}
+                fhirResource={patient}
+                fhirVersion={fhirVersions.R4}
+                patientSimple={false}
+                rawOnClick={() => {
+                    setSelectedPatient(patient);
+                    setView(VIEWS.RESOURCE_EDIT, patient);
+                }}
+            />
+            <div style={{ margin: '-5px 0 20px 15px' }}>
+                <span
+                    className="patientActionButton"
+                    onClick={() => {
+                        setSelectedPatient(patient);
+                        setView(VIEWS.APPOINTMENT_LIST, patient);
+                    }}
+                >
+                    show Appointments
+                </span>
+                <span
+                    className="patientActionButton"
+                    onClick={() => {
+                        setSelectedPatient(patient);
+                        setView(VIEWS.DOCUMENT_REFERENCE_LIST, patient);
+                    }}
+                >
+                    show Documents
+                </span>
+            </div>
+        </li>
+    );
 };
 
 const PatientList = () => {
-  const [patients, setPatients] = useState([]);
-  const setView = useViewStore(state => state.set);
+    const [patients, setPatients] = useState([]);
+    const setView = useViewStore((state) => state.set);
 
-  useEffect(() => {
-    const getPatients = async () => {
-      const data = await fhirclient.request('Patient');
-      setPatients(data);
-    };
-    getPatients();
-  }, []);
+    useEffect(() => {
+        const getPatients = async () => {
+            const data = await fhirclient.request('Patient');
+            setPatients(data);
+        };
+        getPatients();
+    }, []);
 
-  return (
-    <div className='PATIENT'>
-      <h2 className='header'>Patients</h2>
-      <a className="docs" href='https://hl7.org/fhir/R4/patient.html' target='_blank' rel='noreferrer'>&#8505;</a>
-      <span className='addNew' onClick={() => {setView(VIEWS.RESOURCE_EDIT, NEW_PATIENT, {new: true});}}>&#8853;</span>
-      <ul className='list'>
-        {patients.map((patient) => (
-          <PatientCard key={patient.id} patientId={patient.id} />
-        ))}
-      </ul>
-    </div>
-  );
+    return (
+        <div className="PATIENT">
+            <h2 className="header">Patients</h2>
+            <a className="docs" href="https://hl7.org/fhir/R4/patient.html" target="_blank" rel="noreferrer">
+                &#8505;
+            </a>
+            <span
+                className="addNew"
+                onClick={() => {
+                    setView(VIEWS.RESOURCE_EDIT, NEW_PATIENT, { new: true });
+                }}
+            >
+                &#8853;
+            </span>
+            <ul className="list">
+                {patients.map((patient) => (
+                    <PatientCard key={patient.id} patientId={patient.id} />
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default PatientList;
--- client/SomeRandomHIS/src/store.js
+++ client/SomeRandomHIS/src/store.js
@@ -2,25 +2,25 @@ import { create } from 'zustand';
 import { persist, createJSONStorage } from 'zustand/middleware';
 
 export const usePatientStore = create((set) => ({
-  patient: undefined,
-  set: (p) => set(() => ({ patient: p })),
-  remove: () => set({ patient: undefined }),
+    patient: undefined,
+    set: (p) => set(() => ({ patient: p })),
+    remove: () => set({ patient: undefined }),
 }));
 
 export const useViewStore = create(
-  persist(
-    (set) => ({
-      view: {
-        type: undefined,
-        data: undefined,
-        options: {},
-      },
-      set: (type, data, options) => set(() => ({ view: { type, data, options } })),
-      remove: () => set({ view: { type: undefined, data: undefined, options: {} } }),
-    }),
-    {
-      name: 'view-storage',
-      getStorage: () => createJSONStorage(() => localStorage),
-    }
-  )
+    persist(
+        (set) => ({
+            view: {
+                type: undefined,
+                data: undefined,
+                options: {},
+            },
+            set: (type, data, options) => set(() => ({ view: { type, data, options } })),
+            remove: () => set({ view: { type: undefined, data: undefined, options: {} } }),
+        }),
+        {
+            name: 'view-storage',
+            getStorage: () => createJSONStorage(() => localStorage),
+        }
+    )
 );
