fdf484af0187e60013a420836256a2b48f61d05c
Chris Holm Roelsgaard <chris@emento.dk>
2023-06-08 23:11:05 +0200

    add new DocumentReference

--- client/SomeRandomHIS/src/Components/DocumentReferenceList/DocumentReferenceList.jsx
+++ client/SomeRandomHIS/src/Components/DocumentReferenceList/DocumentReferenceList.jsx
@@ -4,58 +4,74 @@ import FHIR from 'fhirclient';
 import './DocumentReferenceList.css';
 import { useViewStore } from '../../store';
 import { VIEWS } from '../../constants';
+import NEW_DOCUMENT_REFERENCE from './NewDocumentReference.json';
 
 const fhirclient = FHIR.client({
-  serverUrl: 'https://76ffhj0j-5173.euw.devtunnels.ms/4_0_0'
+    serverUrl: 'https://76ffhj0j-5173.euw.devtunnels.ms/4_0_0',
 });
 
-const DocumentReferenceCard = ({documentReferenceId}) => {
-  const [documentReference, setDocumentReference] = useState({});
-  const setView = useViewStore(state => state.set);
+const DocumentReferenceCard = ({ documentReferenceId }) => {
+    const [documentReference, setDocumentReference] = useState({});
+    const setView = useViewStore((state) => state.set);
 
-  useEffect(() => {
-    const getData = async () => {
-      const data = await fhirclient.request(`DocumentReference/${documentReferenceId}`);
-      setDocumentReference(data);
-    };
-    getData();
-  }, [documentReferenceId]);
+    useEffect(() => {
+        const getData = async () => {
+            const data = await fhirclient.request(`DocumentReference/${documentReferenceId}`);
+            setDocumentReference(data);
+        };
+        getData();
+    }, [documentReferenceId]);
 
-  return (
-    <li className="item">
-      <DocumentReference
-        style={{width: '100%'}}
-        fhirResource={documentReference}
-        fhirVersion={fhirVersions.R4}
-        patientSimple={false}
-        rawOnClick={() => {setView(VIEWS.RESOURCE_EDIT, documentReference);}}
-      />
-    </li>
-  );
+    return (
+        <li className="item">
+            <DocumentReference
+                style={{ width: '100%' }}
+                fhirResource={documentReference}
+                fhirVersion={fhirVersions.R4}
+                patientSimple={false}
+                rawOnClick={() => {
+                    setView(VIEWS.RESOURCE_EDIT, documentReference);
+                }}
+            />
+        </li>
+    );
 };
 
-const DocumentReferenceList = ({patient}) => {
-  const [documentReferences, setDocumentReferences] = useState([]);
+const DocumentReferenceList = ({ patient }) => {
+    const [documentReferences, setDocumentReferences] = useState([]);
+    const setView = useViewStore((state) => state.set);
 
-  useEffect(() => {
-    const getData = async () => {
-      const data = await fhirclient.request(`DocumentReference?patient=${patient.id}`);
-      setDocumentReferences(data);
-    };
-    getData();
-  }, [patient]);
+    useEffect(() => {
+        const getData = async () => {
+            const data = await fhirclient.request(`DocumentReference?patient=${patient.id}`);
+            setDocumentReferences(data);
+        };
+        getData();
+    }, [patient]);
 
-  return (
-    <div className="DOCUMENTREFERENCE">
-        <h2>Document Reference</h2>
-        <a className="docs" href='https://hl7.org/fhir/R4/documentreference.html' target='_blank' rel='noreferrer'>&#8505;</a>
-        <ul className='list'>
-            {documentReferences.map((documentReference) => (
-                <DocumentReferenceCard key={documentReference.id} documentReferenceId={documentReference.id} />
-            ))}
-        </ul>
-    </div>
-  );
+    return (
+        <div className="DOCUMENTREFERENCE">
+            <h2>Document Reference</h2>
+            <a className="docs" href="https://hl7.org/fhir/R4/documentreference.html" target="_blank" rel="noreferrer">
+                &#8505;
+            </a>
+            <span
+                className="addNew"
+                onClick={() => {
+                    const newDocumentReference = Object.assign({}, NEW_DOCUMENT_REFERENCE);
+                    newDocumentReference.subject.reference = `Patient/${patient.id}`;
+                    setView(VIEWS.RESOURCE_EDIT, NEW_DOCUMENT_REFERENCE, { new: true });
+                }}
+            >
+                &#8853;
+            </span>
+            <ul className="list">
+                {documentReferences.map((documentReference) => (
+                    <DocumentReferenceCard key={documentReference.id} documentReferenceId={documentReference.id} />
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default DocumentReferenceList;
--- client/SomeRandomHIS/src/Components/ResourceEdit/ResourceEdit.jsx
+++ client/SomeRandomHIS/src/Components/ResourceEdit/ResourceEdit.jsx
@@ -5,10 +5,10 @@ import './ResourceEdit.css';
 import FHIR from 'fhirclient';
 
 const fhirclient = FHIR.client({
-    serverUrl: 'https://76ffhj0j-5173.euw.devtunnels.ms/4_0_0'
+    serverUrl: 'https://76ffhj0j-5173.euw.devtunnels.ms/4_0_0',
 });
 
-const ResourceEdit = ({resource, options}) => {
+const ResourceEdit = ({ resource, options }) => {
     const [editObject, setEditObject] = React.useState({});
     const [error, setError] = React.useState();
 
@@ -18,70 +18,87 @@ const ResourceEdit = ({resource, options}) => {
             setEditObject(data);
         };
 
-        if (options && options.new){
+        if (options && options.new) {
             setEditObject(resource);
         } else {
             getData();
         }
     }, [resource, options]);
 
-  return (
-    <div className='RESOURCEEDIT'>
-        <h2 className='header'>Resource Edit</h2>
-        <div className='actions'>
-            <span className='actionButton' onClick={async() => {
-                setError();
-                await fhirclient.update(editObject, {
-                    headers: {
-                        'Content-Type': 'application/fhir+json'
-                    }
-                }).catch((err) => {
-                    setError(err);
-                });
-                window.location.reload();
-            }}>SAVE</span>
-            <span className='actionButton' onClick={async() => {
-                setError();
-                await fhirclient.delete(`${editObject.resourceType}/${editObject.id}`).catch((err) => {
-                    setError(err);
-                });
-                window.location.reload();
-            }}>DELETE</span>
-        </div>
+    return (
+        <div className="RESOURCEEDIT">
+            <h2 className="header">Resource Edit</h2>
+            <div className="actions">
+                <span
+                    className="actionButton"
+                    onClick={async () => {
+                        setError();
+                        await fhirclient
+                            .update(editObject, {
+                                headers: {
+                                    'Content-Type': 'application/fhir+json',
+                                },
+                            })
+                            .catch((err) => {
+                                setError(err);
+                            });
+                        window.location.reload();
+                    }}
+                >
+                    SAVE
+                </span>
+                <span
+                    className="actionButton"
+                    onClick={async () => {
+                        setError();
+                        await fhirclient.delete(`${editObject.resourceType}/${editObject.id}`).catch((err) => {
+                            setError(err);
+                        });
+                        window.location.reload();
+                    }}
+                >
+                    DELETE
+                </span>
+            </div>
 
-        <div className='errorText'>
-            {error && error.message}
+            <div className="errorText">{error && error.message}</div>
+            <div className="container">
+                <JsonEditor
+                    key={editObject.id}
+                    data={editObject}
+                    width="700px"
+                    onChange={(data) => {
+                        setEditObject(data);
+                    }}
+                    optionsMap={{
+                        resourceType: [
+                            { value: 'Patient', label: 'Patient' },
+                            { value: 'Appointment', label: 'Appointment' },
+                            { value: 'DocumentReference', label: 'DocumentReference' },
+                        ],
+                        status: [
+                            { value: 'proposed', label: 'Proposed' },
+                            { value: 'pending', label: 'Pending' },
+                            { value: 'booked', label: 'Booked' },
+                            { value: 'arrived', label: 'Arrived' },
+                            { value: 'fulfilled', label: 'Fulfilled' },
+                            { value: 'cancelled', label: 'Cancelled' },
+                            { value: 'noshow', label: 'No Show' },
+                            { value: 'entered-in-error', label: 'Entered in Error' },
+                            { value: 'checked-in', label: 'Checked In' },
+                            { value: 'waitlist', label: 'Waitlist' },
+                        ],
+                        docStatus: [
+                            { value: 'preliminary', label: 'Preliminary' },
+                            { value: 'final', label: 'Final' },
+                            { value: 'amended', label: 'Amended' },
+                            { value: 'entered-in-error', label: 'Entered in Error' },
+                        ],
+                    }}
+                />
+            </div>
         </div>
-        <div className='container'>
-            <JsonEditor
-                key={editObject.id}
-                data={editObject}
-                width='700px'
-                onChange={data => {
-                    setEditObject(data);
-                }}
-                optionsMap={{
-                    resourceType: [
-                        { value: 'Patient', label: 'Patient' },
-                        { value: 'Appointment', label: 'Appointment' },
-                    ],
-                    status: [
-                        { value: 'proposed', label: 'Proposed' },
-                        { value: 'pending', label: 'Pending' },
-                        { value: 'booked', label: 'Booked' },
-                        { value: 'arrived', label: 'Arrived' },
-                        { value: 'fulfilled', label: 'Fulfilled' },
-                        { value: 'cancelled', label: 'Cancelled' },
-                        { value: 'noshow', label: 'No Show' },
-                        { value: 'entered-in-error', label: 'Entered in Error' },
-                        { value: 'checked-in', label: 'Checked In' },
-                        { value: 'waitlist', label: 'Waitlist' },
-                    ],
-                }}
-            />
-        </div>
-    </div>
-  );
+    );
 };
 
 export default ResourceEdit;
